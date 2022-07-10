ec402565167b410d03f002fb8747a9f570cb7e66
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-07-10 23:09:52 +0430

    mui

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,32 @@
 import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import Input from "@mui/material/Input";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormControl from "@mui/material/FormControl";
+import TextField from "@mui/material/TextField";
+import AccountCircle from "@mui/icons-material/AccountCircle";
 
 function App() {
     return (
-        <div>
-            <Button variant="contained">Hello World</Button>
-        </div>
-    );
+        <Box sx={{ '& > :not(style)': { m: 1 } }}>
+      <FormControl variant="standard">
+        <InputLabel htmlFor="input-with-icon-adornment">
+          With a start adornment
+        </InputLabel>
+        <Input
+          id="input-with-icon-adornment"
+          startAdornment={
+            <InputAdornment position="start">
+              <AccountCircle />
+            </InputAdornment>
+          }
+        />
+      </FormControl>
+
+    </Box>
+  );
+  
 }
 
 export default App;
