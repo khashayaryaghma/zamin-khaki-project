fe01e1af5489ed7f6cf8a6ee001cc75069a4afbf
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-07-14 18:05:49 +0430

    sb

--- client/src/stories/Button.stories.js
+++ client/src/stories/Button.stories.js
@@ -2,7 +2,7 @@ import Buttoon from "../component/Button";
 
 
 export default {
-    title: "Buttoon",
+    title: "Button",
     component: Buttoon,
 };
 
@@ -16,25 +16,3 @@ Primary.args = {
     primary: true,
     label: "Button",
 };
-
-// export const Secondary = Template.bind({});
-// Secondary.args = {
-//     label: "Button",
-// };
-
-// export const Large = Template.bind({});
-// Large.args = {
-//     size: "large",
-//     label: "Button",
-// };
-
-// export const Small = Template.bind({});
-// Small.args = {
-//     size: "small",
-//     label: "Button",
-// };
-// export const FirstStory = {
-//     args: {
-        
-//     },
-// };
