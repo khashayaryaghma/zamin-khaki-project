a4a0d9b5980aa50ea767b0dc2c675869723566f0
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-07-14 12:33:47 +0430

    storybook button

---
+++ client/src/stories/Button.stories.js
@@ -0,0 +1,16 @@
+import Buttoon from "../component/Button";
+
+
+export default {
+    title: "Buttoon",
+    component: Buttoon,
+};
+
+
+const Template = (args) => <Buttoon {...args} />;
+
+export const FirstStory = {
+    args: {
+        
+    },
+};
