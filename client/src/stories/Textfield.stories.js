4b7a5c4b2494d6b0f283f3a8c1d8cbb471837078
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-07-16 21:41:12 +0430

    Create Textfield.stories.js

---
+++ client/src/stories/Textfield.stories.js
@@ -0,0 +1,10 @@
+import Textfieldd from "../component/Textfield";
+
+export default {
+    title: "Textfield",
+    component: Textfieldd,
+};
+
+export const Filled = () => <Textfieldd variant="filled" />;
+export const Outlined = () => <Textfieldd variant="outlined" />;
+export const Standard = () => <Textfieldd variant="standard" />;
