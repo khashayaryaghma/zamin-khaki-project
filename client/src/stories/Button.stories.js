c504688b41b5704b456c2d33b332bbe409824d90
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-07-16 21:25:22 +0430

    Update Button.stories.js

--- client/src/stories/Button.stories.js
+++ client/src/stories/Button.stories.js
@@ -6,12 +6,14 @@ export default {
     component: Buttoon,
 };
 
+export const Sample = (args) => <Buttoon {...args} />;
+export const Primary = () => <Buttoon variant="outlined" />;
+export const Secondary = () => <Buttoon variant="contained" />;
 
-const Template = (args) => <Buttoon {...args} />;
+// const Template = (args) => <Buttoon {...args} />;
 
+// export const Primary = Template.bind({});
 
-export const Primary = Template.bind({});
-
-Primary.args = {
-    label: "Button",
-};
+// Primary.args = {
+//     label: "Button",
+// };
